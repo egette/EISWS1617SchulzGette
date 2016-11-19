e0251ea73b47b1348af88dff0b9dfe3d5c8af88c
egette <enrico.gette@gmail.com>
2016-11-19 18:39:07 +0100

    Kandidaten Funktion des Server erweitert

--- MS1/Server/routes/kandidaten.js
+++ MS1/Server/routes/kandidaten.js
@@ -1,10 +1,30 @@
 exports.getKandidaten = function(db){
 	return function(req, res){
-		var KID = req.query.KID;
 		var wahlkreis = req.query.wahlkreis;
 		var Kandidaten_IDS = [];
-		if(wahlkreis){
-		db.smembers("Kandidaten_"+wahlkreis, function(err, replies){
+		if(req.query.KID){
+			var KID = req.query.KID;
+			db.get(KID, function (err, reply) { 
+				if(err) throw err;
+				if (!reply) {
+					res.status(500).end();
+				} else {
+					var Kandidat = JSON.parse(reply);
+					var result = {
+						username: Kandidat.username,
+						vorname: Kandidat.vorname,
+						nachname: Kandidat.nachname,
+						wahlkreis: Kandidat.wahlkreis,
+						email: Kandidat.email,
+						KID: Kandidat.KID,
+						Partei: Kandidat.Partei,
+						Thesen_beantwortet: Kandidat.Thesen_beantwortet
+					};
+					res.status(200).send(result).end();
+				}
+			});
+		} else if(wahlkreis){
+			db.smembers("Kandidaten_"+wahlkreis, function(err, replies){
 				if( !replies || replies.length==0){
 					//KEINE Kandidaten im Wahlkreis
 				   res.status(401).end();
@@ -17,26 +37,23 @@ exports.getKandidaten = function(db){
 				}	
 			});	
 		}
-	
 	}
-}
+};
 
 function makeKandidatenJSON(Kandidaten_IDS, db){
 	var promises = [];
 	var Kandidaten_JSONOBJECT = { "Kandidaten" : [], "Anzahl": "0" };
 	Kandidaten_JSONOBJECT.Anzahl = Kandidaten_IDS.length;
 
-
-  for (var i = 0; i < Kandidaten_IDS.length; i++) {
-    promises.push(db.getAsync(Kandidaten_IDS[i]))
-  }
+	for (var i = 0; i < Kandidaten_IDS.length; i++) {
+	promises.push(db.getAsync(Kandidaten_IDS[i]))
+	}
 
    return Promise.all(promises).then(function(arrayOfResults) {
-      
       for (i = 0; i < arrayOfResults.length; i++) {
         var kandidatenjsondata = arrayOfResults[i];
         Kandidaten_JSONOBJECT.Kandidaten.push(kandidatenjsondata);
       }
       return Kandidaten_JSONOBJECT;
     });
-}
+};
